98a0ab475060502b5aeb6ce1e5916c2692cd4670
kinglion <1609019405@qq.com>
2020-03-19 15:42:51 +0800

    封装

---
+++ design-patterns/calculator/Operation.cs
@@ -0,0 +1,23 @@
+﻿public class Operation
+{
+    public static double GetResult(double numberA, double numberB, string operate)
+    {
+        double result = 0d;
+        switch(operate)
+        {
+            case "+":
+                result = numberA + numberB;
+                break;
+            case "-":
+                result = numberA - numberB;
+                break;
+            case "*":
+                result = numberA* numberB;
+                break;
+            case "/":
+                result = numberA / numberB;
+                break;
+        }
+        return result;
+    }
+}
