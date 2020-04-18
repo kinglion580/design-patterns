98dae3a019d162d9a3425d3c815a01baadf7ad88
kinglion580 <1609019405@qq.com>
2020-04-18 12:30:13 -0500

    简单工厂模式

---
+++ pos/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pos
+{
+ 
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double total = 0.0d;
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.AddRange(new object[] { "正常收费", "满300减100", "打8折"});
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CashSuper csuper = CashFactory.createCashAccept(comboBox1.SelectedItem.ToString());
+            double totalPrices = 0d;
+            totalPrices = csuper.acceptCash(Convert.ToDouble(textBox1.Text) * Convert.ToDouble(textBox2.Text));
+            total = total + totalPrices;
+            listBox1.Items.Add("单价: " + textBox1.Text + "数量：" + textBox2.Text + " " + comboBox1.SelectedItem + "合计：" + totalPrices.ToString());
+            label5.Text = total.ToString();
+        }
+    }
+}
---
+++ pos/cash.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pos
+{
+    abstract class CashSuper
+    {
+        public abstract double acceptCash(double money);
+    }
+
+
+    class CashNormal : CashSuper
+    {
+        public override double acceptCash(double money)
+        {
+            return money;
+        }
+    }
+
+
+    class CashRebate : CashSuper
+    {
+        private double moneyRebate = 1d;
+
+        public CashRebate(string moneyRebate)
+        {
+            this.moneyRebate = double.Parse(moneyRebate);
+        }
+
+        public override double acceptCash(double money)
+        {
+            return money * moneyRebate;
+        }
+    }
+
+
+    class CashReturn : CashSuper
+    {
+        private double moneyCondition = 0.0d;
+        private double moneyReturn = 0.0d;
+
+        public CashReturn(string moneyCondition, string moneyReturn)
+        {
+            this.moneyCondition = double.Parse(moneyCondition);
+            this.moneyReturn = double.Parse(moneyReturn);
+        }
+
+        public override double acceptCash(double money)
+        {
+            double result = money;
+            if (money >= moneyCondition)
+                result = money - Math.Floor(money / moneyCondition) * moneyReturn;
+
+            return result;
+        }
+    }
+
+
+    class CashFactory
+    {
+        public static CashSuper createCashAccept(string type)
+        {
+            CashSuper cs = null;
+            switch (type)
+            {
+                case "正常收费":
+                    cs = new CashNormal();
+                    break;
+                case "满300减100":
+                    CashReturn cr1 = new CashReturn("300", "100");
+                    cs = cr1;
+                    break;
+                case "打8折":
+                    CashRebate cr2 = new CashRebate("0.8");
+                    cs = cr2;
+                    break;
+            }
+            return cs;
+        }
+    }
+}
