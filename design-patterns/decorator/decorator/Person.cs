e70df14a40b6a2e1656f02271dad15b7053fcd55
kinglion580 <1609019405@qq.com>
2020-04-27 02:04:42 -0500

    wear

---
+++ design-patterns/decorator/decorator/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace decorator
+{
+    class Person
+    {
+        private string name;
+        public Person(string name)
+        {
+            this.name = name;
+        }
+        public void WearTShirts()
+        {
+            Console.Write("大T恤");
+        }
+        public void WearBigTrouser()
+        {
+            Console.Write("垮裤");
+        }
+        public void WearSneakers()
+        {
+            Console.Write("破球鞋");
+        }
+        public void WearSuit()
+        {
+            Console.Write("西装");
+        }
+        public void WearTie()
+        {
+            Console.Write("领带");
+        }
+        public void WearLeatherShoes()
+        {
+            Console.Write("皮鞋");
+        }
+        public void Show()
+        {
+            Console.WriteLine("装扮的{0}", name);
+        }
+    }
+}
---
+++ design-patterns/decorator/decorator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person xc = new Person("小菜");
+
+            Console.WriteLine("\n第一种装扮: ");
+
+            xc.WearTShirts();
+            xc.WearBigTrouser();
+            xc.WearSneakers();
+            xc.Show();
+
+            Console.WriteLine("\n第二种装扮: ");
+            
+            xc.WearSuit();
+            xc.WearTie();
+            xc.WearLeatherShoes();
+            xc.Show();
+
+            Console.Read();
+        }
+    }
+}
