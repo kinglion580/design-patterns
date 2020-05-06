5ccd80eedcef2527bc89c899b954a83c2bb07070
kinglion580 <1609019405@qq.com>
2020-05-06 01:13:23 -0500

    init resume

---
+++ resume/resume/Resume.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace resume
+{
+    class Resume
+    {
+        private string name;
+        private string sex;
+        private string age;
+        private string timeArea;
+        private string company; 
+        
+        public Resume(string name)
+        {
+            this.name = name;
+        }
+
+        public void SetPersonalInfo(string sex, string age)
+        {
+            this.sex = sex;
+            this.age = age;
+        }
+
+        public void SetWorkExperience(string timeArea, string company)
+        {
+            this.timeArea = timeArea;
+            this.company = company;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("{0} {1} {2}", name, sex, age);
+            Console.WriteLine("工作经历: {0} {1}", timeArea, company);
+        }
+    }
+}
