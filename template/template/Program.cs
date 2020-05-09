51fa326fd33b8e9fd94f40ab017bfedeab075876
kinglion580 <1609019405@qq.com>
2020-05-08 21:50:32 -0500

    .....

---
+++ template/template/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace template
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("student a: ");
+            TestPaperA a = new TestPaperA();
+            a.TestQuestion1();
+            a.TestQuestion2();
+            a.TestQuestion3();
+
+            Console.WriteLine("student b: ");
+            TestPaperB b = new TestPaperB();
+            b.TestQuestion1();
+            b.TestQuestion2();
+            b.TestQuestion3();
+
+            Console.Read();
+        }
+    }
+}
---
+++ template/template/TestPaper.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace template
+{
+    class TestPaper
+    {
+        public void TestQuestion1 ()
+        {
+            Console.WriteLine("Question 1");
+        }
+
+        public void TestQuestion2 ()
+        {
+            Console.WriteLine("Question 2");
+        }
+
+        public void TestQuestion3 ()
+        {
+            Console.WriteLine("Question 3");
+        }
+    }
+
+    
+    class TestPaperA : TestPaper
+    {
+        public new void TestQuestion1 ()
+        {
+            base.TestQuestion1();
+            Console.WriteLine("answer: b");
+        }
+
+        public new void TestQuestion2()
+        {
+            base.TestQuestion2();
+            Console.WriteLine("answer: c");
+        }
+
+        public new void TestQuestion3()
+        {
+            base.TestQuestion3();
+            Console.WriteLine("answer: d");
+        }
+    }
+
+
+    class TestPaperB : TestPaper
+    {
+        public new void TestQuestion1()
+        {
+            base.TestQuestion1();
+            Console.WriteLine("answer: a");
+        }
+
+        public new void TestQuestion2()
+        {
+            base.TestQuestion2();
+            Console.WriteLine("answer: b");
+        }
+
+        public new void TestQuestion3()
+        {
+            base.TestQuestion3();
+            Console.WriteLine("answer: c");
+        }
+    }
+}
